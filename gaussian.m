d0db9100f1a5bf8c2b73693eccc837bf3811a9e5
Micah Zirn <micahz@wharton.upenn.edu>
2018-12-02 16:53:21 -0500

    Create gaussian.m

---
+++ gaussian.m
@@ -0,0 +1,18 @@
+function reduced_row_matrix = gaussian(augmented_matrix)
+    [rows, cols] = size(augmented_matrix)
+    
+    for i=1:1:rows
+        [~, index] = max(augmented_matrix(i:rows, i));
+        pivot = i+index-1;
+        extra_row = augmented_matrix(i, :);
+        augmented_matrix(i, :) = augmented_matrix(pivot, :);
+        augmented_matrix(pivot, :) = extra_row;
+        for j=i+1:1:rows
+            factor = augmented_matrix(j, i) / augmented_matrix(i, i);
+            factor_row = factor * augmented_matrix(i, :);
+            augmented_matrix(j, :) = augmented_matrix(j, :) - factor_row;
+        end
+    end
+    
+    reduced_row_matrix = augmented_matrix
+end
