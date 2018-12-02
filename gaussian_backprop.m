1aa9a3dcd5edcdb6b6b526a1d6673f455161c9f6
Micah Zirn <micahz@wharton.upenn.edu>
2018-12-02 17:02:50 -0500

    Update gaussian_backprop.m

--- gaussian_backprop.m
+++ gaussian_backprop.m
@@ -14,13 +14,7 @@ function variable_vals = gaussian_backprop(mtx, solutions)
     augmented_matrix(:, 1:cols) = mtx;
     augmented_matrix(:, cols+1) = solutions;
     augmented_matrix = gaussian(augmented_matrix);
-    variable_vals = zeros(rows+1, 1);
-    for i=rows:-1:1
-        row_v1 = zeros(1, cols+1);
-        row_v1(1, 1:cols) = augmented_matrix(i, 1:cols);
-        d = dot(row_v1, variable_vals);
-        variable_vals(i, 1) = (augmented_matrix(i, cols+1) - d) / augmented_matrix(i, i);
-    end
-    variable_vals = variable_vals(1:rows, 1);
+    
+    variable_vals = upper_triangular_backprop(augmented_matrix);
 end
 
