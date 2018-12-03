4d7f048474726fff9de514928955b5244bb8d158
Micah Zirn <micahz@wharton.upenn.edu>
2018-12-02 22:31:07 -0500

    Update upper_triangular_backprop.m

--- upper_triangular_backprop.m
+++ upper_triangular_backprop.m
@@ -7,6 +7,6 @@ function variable_vals = upper_triangular_backprop(augmented_matrix)
         row_v1 = zeros(1, rows);
         row_v1(1, 1:rows) = augmented_matrix(i, 1:rows);
         d = dot(row_v1, variable_vals);
-        variable_vals(i, 1) = (augmented_matrix(i, cols+1) - d) / augmented_matrix(i, i);
+        variable_vals(i, 1) = (augmented_matrix(i, cols) - d) / augmented_matrix(i, i);
     end
 end    
