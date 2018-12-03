886e89bef8b849f1438bf16e7e2c54ed77fd5aad
Micah Zirn <micahz@wharton.upenn.edu>
2018-12-02 22:24:07 -0500

    Update lower_triangular_backprop.m

--- lower_triangular_backprop.m
+++ lower_triangular_backprop.m
@@ -1,13 +1,12 @@
 function variable_vals = lower_triangular_backprop(augmented_matrix)
     [rows, cols] = size(augmented_matrix)
     
-    variable_vals = zeros(rows+1, 1);
+    variable_vals = zeros(rows, 1);
     
     for i=1:1:rows
-        row_v1 = zeros(1, cols+1);
-        row_v1(1, 1:cols) = augmented_matrix(i, 1:cols);
+        row_v1 = zeros(1, rows);
+        row_v1(1, 1:rows) = augmented_matrix(i, 1:rows);
         d = dot(row_v1, variable_vals);
         variable_vals(i, 1) = (augmented_matrix(i, cols+1) - d) / augmented_matrix(i, i);
     end
-    variable_vals = variable_vals(1:rows, 1);
 end    
